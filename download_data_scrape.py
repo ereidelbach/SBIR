e93069d568799e28df9cd828fd6173d646774a04
eric reidelbach <eric.reidelbach@visionistinc.com>
2018-03-28 08:05:18 -0400

    Finalized first pass of web scraping code (Dict is functioning version)

--- download_data_scrape.py
+++ download_data_scrape.py
@@ -19,21 +19,11 @@ import json
 import requests
 import os  
 import pandas as pd
-from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
 
 '''
-    NOTE 1: For the Selenium driver to function properly on Ubuntu, I had to 
-            download the most up-to-date geckodriver found at:
-            https://github.com/mozilla/geckodriver/releases
-                
-            Once that is complete, extract the driver and place it in the
-            /us/local/bin folder
-           
-    NOTE 2: An effective selenium guide can be found here:
-            https://automatetheboringstuff.com/chapter11/
-            
-            The relevant contents begin roughly 3/4 down the page.
+
 '''
 
 #==============================================================================
@@ -47,42 +37,110 @@ agencyNames = ['Dept. of Defense','Dept. of Health and Human Services',
                'Environmental Protection Agency',
                'Dept. of Commerce','Dept. of Education','Dept. of Transportation',
                'Dept. of Homeland Security']
-
-#  Use the instructions found here to install PhantomJS on Ubuntu: 
-#       https://www.vultr.com/docs/how-to-install-phantomjs-on-ubuntu-16-04
-
-# Phase 1 - SBIR
-#https://www.sbir.gov/sbirsearch/award/all?f[0]=im_field_program:105791&f[1]=im_field_phase:105788
-
-# Phase 2 - SBIR
-url = 'https://www.sbir.gov/sbirsearch/award/all/?f%5B0%5D=im_field_phase%3A105789&f%5B1%5D=im_field_program%3A105791'
-
-# Open a PhantomJS web browser and direct it to the DEA's dropbox search page
-browser = webdriver.PhantomJS()   
-browser.get(url)
-browser.implicitly_wait(100)
+awardInfoNames = ['title','url','agency','branch','contract_num','agency_tracking_num',
+             'award_amount','phase','program','award_year','solicit_year',
+             'solicit_topic_code','solicit_num','comp_name','comp_url',
+             'comp_address','comp_duns','comp_hubzone','comp_wom_owned',
+             'comp_social','comp_pi_name','comp_pi_phone','comp_pi_email',
+             'comp_bus_name','comp_bus_phone','comp_bus_email','abstract']
 
 #==============================================================================
 # Working Code
 #==============================================================================
 
 # Set the project working directory
-os.chdir(r'/home/ejreidelbach/projects/SBIR/Data/Awards_Scraped')
+os.chdir(r'/home/ejreidelbach/projects/SBIR/Data/')
 
-# Use beautifulSoup to extract the download file links for XLS and JSON formats
-html = browser.page_source
-soup = BeautifulSoup(html, 'lxml')
-xlsList = []
-jsonList = []
-for ultag in soup.find_all('ul', {'class': 'dropdown-menu'}):
-    for row in ultag.find_all('div', {'class': 'col-md-5'}):
-        for a in row.select("a[href*=solr_print]"):
-            xlsList.append(a['href'])
-        for a in row.select("a[href*=json_print]"):
-            jsonList.append(a['href'])       
-            
-# Try downloading a file via the headless browser
-dl_url = 'https://www.sbir.gov' + jsonList[0]
+# Get the links for every file in a specific year
+with open('Awards/Combined/awards_2018.json') as json_data:
+    data = json.load(json_data)
+df = pd.DataFrame(data)
+addressList = df['link']
+
+# Scrape all the pages in the year
 headers = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
-r = requests.get(dl_url, headers=headers)
-soup = BeautifulSoup(r.content,'html.parser')               
+
+awardInfo = []
+awardCount = 0
+for url in addressList:
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.content,'html.parser')   
+    
+    # Create container for Award Information
+    tempAwardInfo = []
+    
+    # Grab the Award Title
+    header = soup.find('h1', {'class':'page-header'})
+    tempAwardInfo.append(header.text.encode('ascii','ignore').strip())
+    
+    # Insert the Award URL
+    tempAwardInfo.append(url.encode('ascii','ignore'))
+    
+    # Grab Award Information
+    awardHTML = soup.find('div', {'class':'container-fluid'})
+    for div in awardHTML.find_all('div', {'class':'col-md-6'}):
+        for row in div.find_all('span', {'class':'open-description'}):
+            tempAwardInfo.append(row.text.encode('ascii','ignore').strip())
+            
+    for div in awardHTML.find_all('div', {'class':'col-md-3'}):
+        for row in div.find_all('span', {'class':'open-description'}):
+            tempAwardInfo.append(row.text.encode('ascii','ignore').strip())
+            
+    tempAwardInfo[6] = float(tempAwardInfo[6].strip('$').replace(',',''))
+        
+    # Grab Business Information
+    busHTML = soup.find('div', {'class':'small-business-info-wrapper'})
+    tempAwardInfo.append(busHTML.find('div', {'class':'sbc-name-wrapper'}).text.encode(
+            'ascii','ignore').strip())
+    tempAwardInfo.append('https://www.sbir.gov'+busHTML.find(
+            'a')['href'].encode('ascii','ignore').strip())
+    tempAwardInfo.append(busHTML.find('div', {'class':
+        'sbc-address-wrapper'}).text.strip().encode('ascii','ignore'))
+    for row in busHTML.find_all('span', {'class':'open-description'}):
+        tempAwardInfo.append(row.text.encode('ascii','ignore').strip())
+    
+    busContactHTML = soup.find('div', {'class':'row award-sub-wrapper'})
+    for row in busContactHTML.find_all('div', {'class':'award-sub-description'}):
+        if (row.text.strip() != 'N/A'):
+            try:
+                temp = row.text.encode('ascii','replace').replace(
+                        '?',' ').strip().replace('&nbsp','').replace(';','')
+                # Name
+                tempAwardInfo.append(temp.split('Name:')[1].strip().split('Phone: ')[0])
+                # Phone
+                tempAwardInfo.append(temp.split('Phone: ')[1].strip().split('Email:')[0])
+                # Email
+                tempAwardInfo.append(temp.split('Email:')[1].strip())
+            except:
+                pass
+    
+    # Grab Abstract
+    abstractHTML = soup.find('div',{'class':'abstract-wrapper'})
+    tempAwardInfo.append(abstractHTML.text.encode(
+            'ascii','ignore').strip().replace('\n',' '))
+    awardInfo.append(tempAwardInfo)
+
+    print(awardCount)
+    awardCount+=1
+    
+    time.sleep(5)
+    
+# Convert List to DataFrame
+df2 = pd.DataFrame(awardInfo)
+df2.columns = awardInfoNames
+df2_json = df2.to_json()
+
+# Export the data set
+year = '2018'
+filename = 'Awards_Scraped/awards_' + year + '.json'
+with open(filename, 'wt') as out:
+    out.write(df2_json)
+    
+with open(filename, 'wt') as out:
+    json.dump(awardInfo, out, sort_keys=True, indent=4, separators=(',', ': ')) 
+
+#with open(filename) as json_data:
+#    data = json.load(json_data)
+#with open(filename, 'wt') as out:
+#    json.dump(data, out, sort_keys=True, indent=4, separators=(',', ': ')) 
+
